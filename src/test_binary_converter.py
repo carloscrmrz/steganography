a10c62d7560321210ee749ad9621d94e7a7fc020
Carlos Cabrera <c.cabrera@ciencias.unam.mx>
2022-04-15 15:30:21 -0500

    Added binary decoding test

--- src/test_binary_converter.py
+++ src/test_binary_converter.py
@@ -1,10 +1,15 @@
-import model.binary_converter as bconv
+import model.binary_converter as conv
 
 def test_encode_binary(message):
-    return bconv.encode_binary(message)
+    return conv.encode_binary(message)
+
+def test_decode_binary(bin_message):
+    return conv.decode_binary(bin_message)
+
 
 test_binary = \
   "011010000110010101101100011011000110111100100000011101110110111101110010011011000110010000001010"
 test        = "hello world\n"
 
 assert(test_encode_binary(test) == test_binary)
+assert(test_decode_binary(test_binary) == test)
