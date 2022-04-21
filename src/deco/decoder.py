1715e90d39fd142dbb43850b0f1a9fb0cbf7c10c
sergioccgar <sergioccgar@ciencias.unam.mx>
2022-04-20 20:52:36 -0500

    Primera prueba: Verificar que el archivo que se pretende decodificar sea una imagen y que sea un PNG. El método que se prueba es comprobar(). Funciona.

--- src/deco/decoder.py
+++ src/deco/decoder.py
@@ -1,2 +1,10 @@
-def prueba():
-    print("olaestoesunaprueba")
+from PIL import Image
+
+def comprobar(name):
+    try:
+        img = Image.open(name)
+        t = (img.verify and (img.format == 'PNG'))
+        return t
+    except:
+        t = False
+        return t
--- test/test.py
+++ test/test.py
@@ -3,4 +3,24 @@ sys.path.append("..")
 
 from src.deco import decoder as dec
 
-dec.prueba()
+print("Aquí verificamos que al intentar abrir un archivo",
+        "este, primero, sea una imagen y, segundo, sea PNG.\n")
+
+archivos = ["panda.txt","perro.pdf","gato.png","pez.jpg",
+            "ballena.mp3", "ganso.mp4", "hiena.apk",
+            "koala.png", "lobo.docx", "pingu.png"]
+
+verdad = [False, False, True, False, False, False, False,
+          True, False, True]
+
+verdadaprueba = list()
+
+for name in archivos:
+    verdadaprueba.append(dec.comprobar(name))
+
+for i in range (10):
+    if (verdad[i] == verdadaprueba[i]):
+        print ('\033[92m¡Éxito!')
+    else:
+        print ('\033[93mFracaso')
+    print ('\033[0m')
