a0829016b576c8671f9293440539a6147d8a7cb3
Carlos Cabrera <c.cabrera@ciencias.unam.mx>
2022-04-27 12:21:09 -0500

    Fixed test

--- src/test_binary_converter.py
+++ src/test_binary_converter.py
@@ -1,7 +1,7 @@
 import model.binary_converter as conv
 
-def test_encode_binary(message):
-    return conv.encode_binary(message)
+def test_encode_binary(message,flag):
+    return conv.encode_binary(message,flag)
 
 def test_decode_binary(bin_message):
     return conv.decode_binary(bin_message)
@@ -11,5 +11,5 @@ test_binary = \
   "011010000110010101101100011011000110111100100000011101110110111101110010011011000110010000001010"
 test        = "hello world\n"
 
-assert(test_encode_binary(test) == test_binary)
+assert(test_encode_binary(test,4) == test_binary)
 assert(test_decode_binary(test_binary) == test)
